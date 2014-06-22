e86cfaceece7b86a435389fdd73edac2a49eacb5
cydonio <flex106@yahoo.com>
2014-06-22 21:28:22 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,12 @@
-## These two functions handle the creation of a matrix object which includes the calculation and caching of an
+## The following two functions handle the creation of a matrix object which includes the calculation and caching of an
 ## inverse matrix
 
+
 ## This function contains the get and set operations for the original matrix and the inverse matrix
-## set - stores a the provided matrix in x
+## set - stores the provided matrix in x
 ## get - returns original matrix
 ## setinv - stores the inverse matrix in cache
 ## getinv - returns the inverse matrix from cache
-
 makeCacheMatrix <- function(x = matrix()) {
         mi <- NULL
         set <- function(y) {
@@ -21,9 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
              getinv = getinv)
 }
 
+
 ## This function checks if the inverse matrix has already been cached. If so, it returns the cached matrix
 ## otherwise it calculates the inverse, stores it in the cache and returns it to the caller
-
 cacheSolve <- function(x, ...) {
         inv <- x$getinv()
         if(!is.null(inv)) {
